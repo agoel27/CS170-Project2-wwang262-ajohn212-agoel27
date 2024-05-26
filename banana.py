adb5320e4048af639a6a01a442911cb055443e67
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2024-05-25 17:46:27 -0700

    backward selection

--- banana.py
+++ banana.py
@@ -2,9 +2,13 @@ import random
 
 # instance has id, label, and feature values
 
-def expand(features, numFeatures):
+def expand_forward(features, numFeatures):
     a = {x for x in range(1,numFeatures+1)}
     return {frozenset(features | {x}) for x in a.difference(features)}
+
+def expand_backward(features):
+    return {frozenset(features - {x}) for x in features}
+
 
 def random_eval():
     return random.random()
@@ -12,7 +16,21 @@ def random_eval():
 def forward_selection(numFeatures):
     features = set()
     for i in range(0,numFeatures):
-        features = expand(features, numFeatures)
+        features = expand_forward(features, numFeatures)
+        maxVal = 0
+        bestFeature = None
+        for x in features:
+            eval = random_eval()
+            if eval > maxVal:
+                maxVal = eval
+                bestFeature = x
+        features = bestFeature
+        print(features, maxVal)
+
+def backward_selection(numFeatures):
+    features = {x for x in range(1,numFeatures+1)}
+    for i in range(0,numFeatures-1):
+        features = expand_backward(features)
         maxVal = 0
         bestFeature = None
         for x in features:
@@ -23,14 +41,16 @@ def forward_selection(numFeatures):
         features = bestFeature
         print(features, maxVal)
         
-forward_selection(10)
+# forward_selection(10)
+backward_selection(10)
 
 
 
 
 
 
-# print(expand(set(), 5))
+# print(expand_backward({1,2,3,4,5,6}))
+# print(expand_forward(set(), 5))
 # print(random_eval())
 
 
